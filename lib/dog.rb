82111e266e4ad035b2f598bbf2a52d4cc3a3b082
joey <pball626@gmail.com>
2021-02-24 20:39:42 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+    def name=(name)
+        @name = name
+    end    
+    
+    def name
+        @name 
+    end
+
+    fido= Dog.new
+    fido.name = "Fido"
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    fido.breed = "Rottweiler"
+
+
+end
